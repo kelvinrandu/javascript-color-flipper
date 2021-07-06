e83207b1c834bd33a999d4595d048a72635d3f0e
kelvinrandu <randukelvin@gmail.com>
2021-07-06 15:57:56 +0300

    tweaked colormode in app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const colors = ["green","red","rgba(133,122,200)","#f15025"];
+const hex =[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click',function(){   
-    const random = Math.floor(Math.random()*colors.length)
-    document.body.style.backgroundColor= colors[random];
+btn.addEventListener('click',function(){  
+    let hexColor = '#';
+    for(let i = 0; i<6; i++) {
+        hexColor += hex[0];
+
+    }
+    document.body.style.backgroundColor= hexColor;
+    color.textContent= hexColor;
+   
+    // const random = Math.floor(Math.random()*colors.length)
+    // document.body.style.backgroundColor= colors[random];
+    // color.textContent= colors[random];
    
 })
